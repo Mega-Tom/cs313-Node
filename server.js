e1636c77823c9e8fe1207322d08894840acd67ea
Mega-Tom <thomas@peckfam.com>
2018-11-16 18:18:26 -0700

    more slashes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-app.use("static", express.static("static"))
-   .use("calc", require("./calc.js"))
+app.use(express.static("static"))
+   .use("/calc", require("./calc.js"))
    .set('views', 'views')
    .set('view engine', 'ejs')
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
