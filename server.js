abba4088d32cff8d51d9dfe3e303acc096fee991
Mega-Tom <thomas@peckfam.com>
2018-11-16 18:17:09 -0700

    ./calc.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 var app = express();
 
 app.use("static", express.static("static"))
-   .use("calc", require("calc"))
+   .use("calc", require("./calc.js"))
    .set('views', 'views')
    .set('view engine', 'ejs')
    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
